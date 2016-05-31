cd3b35558f8c7403431f83cb9eae16fe5440fd50
Vincent Sitzmann <sitzmann@cs.stanford.edu>
2016-05-31 15:43:18 -0700

    More helper functions.

--- elasticsearch_helper.py
+++ elasticsearch_helper.py
@@ -7,10 +7,13 @@ def constrain_element(element, constraints):
 def constant_score(query_body):
     return {'constant_score':{'query':query_body}}
 
+
+# Aggregators
 def bool(*relations):
     result = {'bool':{}}
     for relation in relations:
         result['bool'].update(relation)
+
     return result
 
 def must(*relations):
@@ -25,8 +28,12 @@ def must_not(*relations):
     result = {'must_not':list(relations)}
     return result
 
+#
 def match(field_name, field_value):
     return {'match':{field_name:field_value}}
+
+def term(field_name, field_value):
+    return {'term':{field_name:field_value}}
 
 def multi_match(field_names, field_value):
     result = {
@@ -36,4 +43,11 @@ def multi_match(field_names, field_value):
         }
     }
     return result
+
+def range_from_to(field_name, from_value, to_value):
+    return {'range':{field_name:{'from': from_value, 'to': to_value}}}
+
+# Modifiers
+def bool_minimum_should_match(minimum_should_match):
+    return {'minimum_should_match':minimum_should_match}
 
