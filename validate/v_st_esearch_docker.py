5251e5651739cfae33b08f63ef4f31ed538e1113
Vincent Sitzmann <sitzmann@cs.stanford.edu>
2016-06-14 20:50:17 -0700

    Updated to use new functions.

--- validate/v_st_esearch_docker.py
+++ validate/v_st_esearch_docker.py
@@ -4,15 +4,9 @@ import sys, os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from pprint import pprint
-import st_api
+import st_client
 import pandas
-import st_docker
-import json
-import time
-
-# Logging may be switched on for debugging.
-# import logging
-# logging.basicConfig(level=logging.DEBUG)
+from elasticsearch_helper import *
 
 __author__ = 'vsitzmann'
 
@@ -29,75 +23,62 @@ try:
 except Exception:
     pass
 
-scitran_instance = st_api.InstanceHandler('scitran', st_dir)
+scitran_instance = st_client.ScitranClient('scitran', st_dir)
 
 # Start a search
 print("Running the search...")
-search_results = scitran_instance.search_acquisition_files(file_props={'name':'.nii.gz'},
-                                                           acq_props={'measurement':'anatomy_t1w'},
-                                                           collection_labels=['testing'])
+constraints = {}
+constraints.update(constrain_element('acquisitions', match('measurement', 'anatomy_t1w')))
+constraints.update(constrain_element('files', match('type', 'nifti')))
+constraints.update(constrain_element('collections', match('label', 'testing')))
+search_results = scitran_instance.search_files(constraints)
+
 search_results_df = pandas.io.json.json_normalize(search_results)
 print("The search returned %d results."%len(search_results_df))
 
-# Choose one of the files and extract its properties
-test_file = search_results_df.iloc[0]
-container_id = test_file['_source.container_id']
-container_name = test_file['_source.container_name']
-filename = test_file['_source.name']
-
-# This code may be used to find the acquisition that that the file originates from for testing purposes.
-path = 'acquisitions'
-constraints = {
-                'acquisitions':{
-                    'match':{
-                        '_id':container_id
-                    }
-                }
-            }
-source_acquisition = scitran_instance.search(path, constraints)['acquisitions'][0]
-print("Chose the file %s of acquisition %s as a test file."%(filename, source_acquisition['_source']['label']))
-
-# Download the test file to the input directory defined above
-print("Downloading the test file...")
-dest_dir = in_dir
-downloaded_file_path = scitran_instance.download_file(container_id=container_id,
-                                                      container_type=container_name,
-                                                      file_name=filename,
-                                                      dest_dir=dest_dir)
-
-# Start the docker container
-print("Starting the docker vistalab/bet container to run on the test file.")
-in_file = filename
-out_file = in_file[:-7] + '_bet'
-command ='/input/%s /output/%s'%(in_file, out_file)
-st_docker.run_container('vistalab/bet', command=command, in_dir=in_dir, out_dir=out_dir)
+filenames = scitran_instance.download_all_file_search_results(search_results, dest_dir=in_dir)
 
 # Find one collection that the file belongs to:
-file_collections = scitran_instance.search(path='collections', constraints={'collections':{'match':{'label': 'testing'}}})['collections']
+file_collections = scitran_instance.search_collections(constraints=constrain_element('collections', match('label', 'testing')))
 collection_id = file_collections[0]['_id']
 
-# Reupload the file as an analysis for  the determined collection:
-print("Reuploading result to collection %s."%(file_collections[0]['_source']['label']))
-in_file_path = os.path.join(in_dir, in_file)
-out_file_path = os.path.join(out_dir, out_file + '.nii.gz')
-metadata = {'label':'bet analysis'}
-response = scitran_instance.upload_analysis(in_file_path, out_file_path, metadata, target_collection_id=collection_id)
-analyses_id = json.loads(response.text)['_id']
-print("Uploaded the analyses. Saved in database under ID %s"%analyses_id)
+scitran_instance.run_gear_and_upload_analysis(metadata_label='bet analyses',
+                                              container='vistalab/bet',
+                                              target_collection_id=collection_id,
+                                              in_dir=in_dir,
+                                              out_dir=out_dir)
+file_collections = scitran_instance.search_collections(constraints=constrain_element('collections', match('label', 'testing')))
+pprint(file_collections)
+
+# # Start the docker container
+# print("Starting the docker vistalab/bet container to run on the test file.")
+# in_file = filename
+# out_file = in_file[:-7] + '_bet'
+# command ='/input/%s /output/%s'%(in_file, out_file)
+# st_docker.run_container('vistalab/bet', command=command, in_dir=in_dir, out_dir=out_dir)
+#
+# # Reupload the file as an analysis for  the determined collection:
+# print("Reuploading result to collection %s."%(file_collections[0]['_source']['label']))
+# in_file_path = os.path.join(in_dir, in_file)
+# out_file_path = os.path.join(out_dir, out_file + '.nii.gz')
+# metadata = {'label':'testing_multipart'}
+# response = scitran_instance.upload_analysis(in_file_path, out_file_path, metadata, target_collection_id=collection_id)
+# analyses_id = json.loads(response.text)['_id']
+# print("Uploaded the analyses. Saved in database under ID %s"%analyses_id)
 
 # Find the uploaded analyses. It will take some time for elastic search to index the freshly uploaded file, hence the delay.
-import time
-time.sleep(5) # delays for 5 seconds
+# import time
+# time.sleep(5) # delays for 5 seconds
 
-print("Searching the remote for the freshly uploaded analysis...")
-constraints = {
-    'analyses': {
-        'match': {
-            '_id':analyses_id
-        }
-    }
-}
-
-path='analyses'
-uploaded_analyses = scitran_instance.search(path, constraints)['analyses']
-pprint(uploaded_analyses)
+# print("Searching the remote for the freshly uploaded analysis...")
+# constraints = {
+#     'analyses': {
+#         'match': {
+#             '_id':analyses_id
+#         }
+#     }
+# }
+#
+# path='analyses'
+# uploaded_analyses = scitran_instance.search(path, constraints)['analyses']
+# pprint(uploaded_analyses)
