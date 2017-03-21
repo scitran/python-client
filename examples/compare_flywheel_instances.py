1b2f39873d4724444a8d9ea6b24eb5982a279431
Carlos Correa <carloscorrea137@gmail.com>
2017-03-20 18:14:15 -0700

    some notes and tweaks from usage

--- examples/compare_flywheel_instances.py
+++ examples/compare_flywheel_instances.py
@@ -63,6 +63,40 @@ if __name__ == '__main__':
     for s_st, s_cni in sorted_zip(sessions_st, sessions_cni, '_id'):
         if _clean_session(s_st) != _clean_session(s_cni):
             print s_st['_id'], 'sessions different'
+
+        '''
+        an = request(st, 'sessions/{}'.format(s_st['_id'])).get('analyses')
+        # https://flywheel.scitran.stanford.edu/api/sessions/57a163957023e50021e2f952/analyses/58c0a684609b3800136fa569?root=true
+        for analysis in an:
+            num_outputs = len([f for f in analysis['files'] if f.get('output')])
+            # if not analysis['job']['saved_files'] and not num_outputs:
+            if not num_outputs:
+                # xxx add root=true
+                print '{}, saved {}, num out {} DELETE "sessions/{}/analyses/{}?root=true"'.format(
+                    analysis['job']['name'],
+                    len(analysis['job']['saved_files']),
+                    num_outputs,
+                    s_st['_id'],
+                    analysis['_id'],
+                )
+        continue
+        '''
+
+        try:
+            analyses_st = request(st, 'sessions/{}'.format(s_st['_id'])).get('analyses')
+            analyses_cni = request(cni, 'sessions/{}'.format(s_cni['_id'])).get('analyses')
+        except Exception as e:
+            print 'eeek', s_st['_id'], str(e)
+            continue
+        for an_st, an_cni in sorted_zip(analyses_st, analyses_cni, '_id'):
+            if an_st != an_cni:
+                print 'analysis different, sess {} analysis {}'.format(
+                    s_st['_id'],
+                    an_st['_id'],
+                )
+            for f_st, f_cni in sorted_zip(an_st['files'], an_cni['files'], 'name'):
+                all_files.append((s_st, an_st, f_st, 'analysis'))
+
         acq_st = request(st, 'sessions/{}/acquisitions'.format(s_st['_id']))
         acq_cni = request(cni, 'sessions/{}/acquisitions'.format(s_cni['_id']))
         for a_st, a_cni in sorted_zip(acq_st, acq_cni, '_id'):
@@ -78,15 +112,19 @@ if __name__ == '__main__':
                         a_st['_id'],
                         f_st['name'],
                     )
-                all_files.append((s_st, a_st, f_st))
+                all_files.append((s_st, a_st, f_st, 'acquisitions'))
 
     print 'checked', len(all_files), 'files'
 
-    for s, a, f in random.sample(all_files, 100):
-        cni.download_file('acquisitions', a['_id'], f['name'], f['hash'])
+    for s, a, f, container_type in random.sample(all_files, 100):
+        if container_type == 'acquisitions':
+            cni.download_file(container_type, a['_id'], f['name'], f['hash'])
+        else:
+            cni.download_file('sessions', s['_id'], f['name'], f['hash'], analysis_id=a['_id'])
         # if it doesn't match, we'll throw here.
-        print 'file download matches, sess {} acq {} file {}'.format(
+        print 'file download matches, sess {} {} {} file {}'.format(
             s['_id'],
+            container_type,
             a['_id'],
             f['name'],
         )
