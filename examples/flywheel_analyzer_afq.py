90e65f2a583adf5ee9c3243811649e5d20016481
Carlos Correa <carloscorrea137@gmail.com>
2017-03-24 20:58:01 -0700

    add better prefix matcher

--- examples/flywheel_analyzer_afq.py
+++ examples/flywheel_analyzer_afq.py
@@ -5,6 +5,14 @@ import re
 client = ScitranClient('https://flywheel-cni.scitran.stanford.edu')
 
 
+def prefix_matcher(prefix):
+    # doing this funny prefix matching to catch both "afq" and "afq 2017-01-01..."
+    return lambda val: val == prefix or val.startswith(prefix + ' ')
+
+dtiinit_matcher = prefix_matcher('dtiinit')
+afq_matcher = prefix_matcher('afq')
+
+
 def dtiinit_inputs(acquisitions, **kwargs):
     diffusion = fa.find(acquisitions, label='DTI 2mm b1250 84dir(axial)')
 
@@ -16,7 +24,7 @@ def dtiinit_inputs(acquisitions, **kwargs):
 
 
 def afq_inputs(analyses, **kwargs):
-    dtiinit = fa.find(analyses, label='dtiinit')
+    dtiinit = fa.find(analyses, label=dtiinit_matcher)
 
     return dict(
         dtiInit_Archive=dtiinit.find_file('dtiInit_*.zip'),
@@ -24,12 +32,11 @@ def afq_inputs(analyses, **kwargs):
 
 if __name__ == '__main__':
     with fa.installed_client(client):
-        # doing this funny prefix matching to catch both "afq" and "afq 2017-01-01..."
         fa.run([
             fa.define_analysis(
                 'dtiinit', dtiinit_inputs,
-                label_matcher=lambda val: re.match(r'dtiinit($| )', val)),
+                label_matcher=dtiinit_matcher),
             fa.define_analysis(
                 'afq', afq_inputs,
-                label_matcher=lambda val: re.match(r'afq($| )', val)),
+                label_matcher=afq_matcher),
         ], project=fa.find_project(label='ENGAGE'))
