f925d9e44b66d96642aa463589caae169265e837
Carlos Correa <carloscorrea137@gmail.com>
2017-03-29 00:52:57 -0700

    add support for 2month visits borrowing T1 from BV

--- examples/flywheel_analyzer_engage.py
+++ examples/flywheel_analyzer_engage.py
@@ -1,6 +1,26 @@
 import scitran_client.flywheel_analyzer as fa
 from scitran_client import ScitranClient
 
+client = ScitranClient('https://flywheel-cni.scitran.stanford.edu')
+with fa.installed_client(client):
+    project = fa.find_project(label='ENGAGE')
+    sessions = client.request('projects/{}/sessions'.format(project['_id'])).json()
+    session_by_subject = {}
+    second_to_first_visit_id = {}
+    for s in sessions:
+        subject = s['subject']['code'][:7].upper()
+        session_by_subject.setdefault(subject, []).append(s)
+    for subject, subject_sessions in session_by_subject.iteritems():
+        # we need at least two sessions
+        if len(subject_sessions) < 2:
+            continue
+        subject_sessions.sort(key=lambda s: s['timestamp'])
+        # HACK this is a bit of a heuristic. this will certainly fail
+        # for some folks that skipped a BV, or folks that missed the 2mo
+        # but hopefully, those folks will otherwise have data that is just
+        # fine.
+        second_to_first_visit_id[subject_sessions[1]['_id']] = subject_sessions[0]['_id']
+
 
 # XXX at least make this be just the first thing without ' 2'?
 label_to_task_type = {
@@ -39,9 +59,17 @@ def define_analysis(gear_name, acquisition_label, create_inputs):
         label=analysis_label(gear_name, acquisition_label))
 
 
-def reactivity_inputs(acquisition_label, acquisitions, **kwargs):
+def reactivity_inputs(acquisition_label, acquisitions, session, **kwargs):
     functional = fa.find(acquisitions, label=acquisition_label)
     structural = fa.find(acquisitions, label='T1w 1mm')
+    if not structural:
+        assert session['_id'] in second_to_first_visit_id,\
+            'the only sessions that should be missing T1w are second visits. {} was missing a T1w'\
+            .format(session['_id'])
+        first_visit_session_id = second_to_first_visit_id[session['_id']]
+        first_visit_acquisitions = client.request(
+            'sessions/{}/acquisitions'.format(first_visit_session_id)).json()
+        structural = fa.find(first_visit_acquisitions, label='T1w 1mm')
 
     return dict(
         functional=functional.find_file('*.nii.gz'),
@@ -81,7 +109,7 @@ def first_level_model_inputs(acquisition_label, analyses, acquisitions):
     ), dict(task_type=label_to_task_type[acquisition_label])
 
 if __name__ == '__main__':
-    with fa.installed_client(ScitranClient('https://flywheel-cni.scitran.stanford.edu')):
+    with fa.installed_client(client):
         fa.run([
             define_analysis('reactivity-preprocessing', 'go-no-go 2', reactivity_inputs),
             define_analysis('connectivity-preprocessing', 'go-no-go 2', connectivity_inputs),
@@ -98,4 +126,4 @@ if __name__ == '__main__':
             define_analysis('reactivity-preprocessing', 'EmoReg', reactivity_inputs),
             define_analysis('connectivity-preprocessing', 'EmoReg', connectivity_inputs),
             # define_analysis('first-level-models', 'EmoReg', first_level_model_inputs),
-        ], project=fa.find_project(label='ENGAGE'))
+        ], project=project)
