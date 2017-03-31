088ff504bd02969f4bd026ec2ce1afc675775f60
Carlos Correa <carloscorrea137@gmail.com>
2017-03-30 19:43:37 -0700

    draft of changes for new flywheel project

--- examples/flywheel_analyzer_showdes.py
+++ examples/flywheel_analyzer_showdes.py
@@ -8,17 +8,9 @@ with fa.installed_client(client):
         client.request('sessions/588bd1ac449f9800159305c2/acquisitions').json(),
         label='atlas')
 
-    # generate a label for these "Logothetis DES" sessions that matches the
-    # "showdes" sessions.
-    functional_acquisitions_by_session_label = {
-        s['label'].replace('.', '').lower():
-        client.request('sessions/{}/acquisitions'.format(s['_id'])).json()
-        for s in client.request('projects/584b0330bf6dd80015881071/sessions').json()
-    }
-
 
 def anatomical_warp_inputs(acquisitions, **kwargs):
-    anatomical = fa.find(acquisitions, label='anatomical_mdeft')
+    anatomical = fa.find(acquisitions, label='anatomical_nifti')
     return dict(
         native=anatomical.find_file('*.nii.gz'),
         standard=D99.find_file('D99_template.nii.gz'),
@@ -27,11 +19,11 @@ def anatomical_warp_inputs(acquisitions, **kwargs):
 
 
 def functional_warp_inputs(idx, acquisitions, session, analyses, **kwargs):
-    functional_acquisitions = functional_acquisitions_by_session_label[session['label']]
-    label = 'functional_{}'.format(idx)
-    func = fa.find(functional_acquisitions, label=label)
+    name = '{}_{:04}_tcimg.nii.gz'.format(session['label'], idx)
+    functionals = fa.find(acquisitions, label='functionals')
+    func = functionals.find_file(name, default=None)
     if not func:
-        raise fa.SkipOperation('Could not find functional {}'.format(label))
+        raise fa.SkipOperation('Could not find functional {}'.format(name))
 
     anat_warp = fa.find(analyses, label='afni-brain-warp')
     base_anat = '{}_0001_mdeft'.format(session['label'])
@@ -39,7 +31,7 @@ def functional_warp_inputs(idx, acquisitions, session, analyses, **kwargs):
     return dict(
         anatomical=anat_warp.find_file('{}.nii'.format(base_anat)),
         warp_target=anat_warp.find_file('{}_seg.nii'.format(base_anat)),
-        functional=func.find_file('*.nii.gz'),
+        functional=func,
     )
 
 if __name__ == '__main__':
